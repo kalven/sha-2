c9ee06a98aa57ee24e73a395dc4024faf3db1fd2
Daniel Andersson <kalven@gmail.com>
2012-08-24 14:45:13 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sha-2
+
+Public domain SHA-256 processor in C++ without external dependencies. Adapted from [LibTomCrypt](http://libtom.org/?page=features&whatfile=crypt).
+
+## Usage
+
+Compile and link sha256.cpp to your application. The sum.cpp example program emulates coreutils sha256sum.
+
+## License
+
+Sha-2 is public domain.
---
+++ sha256.hpp
@@ -0,0 +1,16 @@
+// SHA-256. Adapted from LibTomCrypt. This code is Public Domain
+#pragma once
+
+#include <cstdint>
+
+struct sha256_state
+{
+    std::uint64_t length;
+    std::uint32_t state[8];
+    std::uint32_t curlen;
+    unsigned char buf[64];
+};
+
+void sha256_init(sha256_state& md);
+void sha256_process(sha256_state& md, const void* in, std::uint32_t inlen);
+void sha256_done(sha256_state& md, void* out);
